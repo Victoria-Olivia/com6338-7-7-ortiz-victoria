6efca96bbb691be14bb6edc7e71ab486c40de4f2
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-07-03 20:49:26 -0400

    Added initial quiz questions and answers

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+var questionsArr = [
+  {
+    question: 'Who directed the film "E.T. the Extra-Terrestrial"?',
+    answer: 'Steven Spielberg',
+    options: [
+      'James Cameron',
+      'Alfred Hitchcock',
+      'Quentin Tarantino',
+      'Steven Spielberg',
+    ]
+  },
+  {
+    question: 'Who directed the movie "Titanic"?',
+    answer: 'James Cameron',
+    options: [
+      'Steven Spielberg',
+      'Martin Scorsese',
+      'James Cameron',
+      'Ridley Scott',
+    ]
+  },
+  {
+    question: 'Who was the director for the first "Alien" film?',
+    answer: 'Ridley Scott',
+    options: [
+      'James Cameron',
+      'Ridley Scott',
+      'George Lucas',
+      'Steven Spielberg',
+    ]
+  },
+  {
+    question: 'Who wrote and directed the movie "Inception"?',
+    answer: 'Christopher Nolan',
+    options: [
+      'Steven Spielberg',
+      'Christopher Nolan',
+      'J.J. Abrams',
+      'Alfred Hitchcock',
+    ]
+  },
+  {
+    question: 'Who directed the first two "Terminator" films starring Arnold Schwarzenegger?',
+    answer: 'James Cameron',
+    options: [
+      'Christopher Nolan',
+      'James Cameron',
+      'Kathryn Bigelow',
+      'Kevin Williamson',
+    ]
+  }
+];
